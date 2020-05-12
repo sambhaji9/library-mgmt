452c6db660ce06f8cacbdc9545045507392295d9
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-05-12 18:30:44 +0200

    Added studentId in all the collections

--- backend/util/insert-data.js
+++ backend/util/insert-data.js
@@ -5,61 +5,19 @@ mongoClient.connect(url, function(err, database) {
     if (err) throw err;
 
     var dbo = database.db("library");
-    // dbo.collection('mathematics').insertMany([{
-    //     "name": "Encyclopedia of Mathematics",
-    //     "language": "English",
-    //     "description": "Encompasses articles on the topic of mathematics",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "Encyclopedia of Triangle Centers",
-    //     "language": "English",
-    //     "description": "Information about ten thousands of triangle centers, including Coordinate systemcoordinates, Operation (mathematics)operations, Incidence (geometry)incidence, equations, etc.",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "EqWorld",
-    //     "language": "English, Russian",
-    //     "description": "Articles on mathematical equations",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "MathWorld",
-    //     "language": "English",
-    //     "description": "Articles on a wide variety of mathematics related topics",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "On-Line Encyclopedia of Integer Sequences",
-    //     "language": "English",
-    //     "description": "Information about hundred thousands of integer sequences, including formulas, related sequences, Computer programprograms, etc. ",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "PlanetMath",
-    //     "language": "English",
-    //     "description": "Mathematics related topics and articles",
-    //     "availability": true
-    // },
-    // {
-    //     "name": "nLab",
-    //     "language": "English",
-    //     "description": "Category theory and related topics",
-    //     "availability": true
+    // dbo.collection('mathematics').insertMany([{"name": "Encyclopedia of Mathematics","language": "English","description": "Encompasses articles on the topic of mathematics","availability": true, "studentId": ""},{"name": "Encyclopedia of Triangle Centers","language": "English","description": "Information about ten thousands of triangle centers, including Coordinate systemcoordinates, Operation (mathematics)operations, Incidence (geometry)incidence, equations, etc.","availability": true, "studentId": ""},{"name": "EqWorld","language": "English, Russian","description": "Articles on mathematical equations","availability": true, "studentId": ""},{"name": "MathWorld","language": "English","description":"Articles on a wide variety of mathematics related topics","availability": true, "studentId": ""},{"name": "On-Line Encyclopedia of Integer Sequences","language": "English","description": "Information about hundred thousands of integer sequences, including formulas, related sequences, Computer programprograms, etc. ","availability": true, "studentId": ""},{"name": "PlanetMath","language": "English","description": "Mathematics related topics and articles","availability": true, "studentId": ""},{"name": "nLab","language": "English","description": "Category theory and related topics","availability": true, "studentId": ""
     // }], function(err, results) {
     //     if (err) throw err;
-
     //     console.log(results.result.n + " documents inserted");
-        
-    //     database.close();
     // });
 
-    // dbo.collection('music').insertMany([{"name": "Bulgarian Rock Archives","language": "Bulgarian language, Bulgarian","description": "Archives of Bulgarian rock musicrock bands","availability": true}, {"name": "Encyclopaedia Metallum","language": "English","description": "Archives of heavy musicheavy metal bands","availability": true},{"name": "Grove Music Online","language": "English","description": "Mostly on historic and classical music worldwide; in print form is ''The New Grove Dictionary of Music and Musicians'', which has 20 printed volumes, and was first published in 1980","availability": true},{"name": "MOOMA","language": "Hebrew language, Hebrew","description": "Articles on Israeli music and musical artists","availability": true}], function(err, results) {
+    // dbo.collection('music').insertMany([{"name": "Bulgarian Rock Archives","language": "Bulgarian language, Bulgarian","description": "Archives of Bulgarian rock musicrock bands","availability": true, "studentId": ""}, {"name": "Encyclopaedia Metallum","language": "English","description": "Archives of heavy musicheavy metal bands","availability": true, "studentId": ""},{"name": "Grove Music Online","language": "English","description": "Mostly on historic and classical music worldwide; in print form is ''The New Grove Dictionary of Music and Musicians'', which has 20 printed volumes, and was first published in 1980","availability": true, "studentId": ""},{"name": "MOOMA","language": "Hebrew language, Hebrew","description": "Articles on Israeli music and musical artists","availability": true, "studentId": ""}], function(err, results) {
+    //     if (err) throw err;
     //     console.log(results.insertedCount + " documents inserted successfully");
     // });
 
-    // dbo.collection('biography').insertMany([{"name": "Australian Dictionary of Biography","language": "English","description": "Entries on notable Australians who have died", "availability": true },{"name": "Croatian Biographical Lexicon","language": "Croatian languageCroatian","description": "Multi-volume biographical reference work on notable figures from Croatian history", "availability": true },{"name": "Deutsche Biographie","language": "German languageGerman","description": "Biographies on notable German speaking people", "availability": true },{"name": "Dictionary of Canadian Biography","language": "English, French","description": "Entries on notable Canadians", "availability": true },{"name": "Dictionary of Irish Architects","language": "English","description": "Biographical information on Irish architects from 1720 to 1940","availability": true },{"name": "Kdo byl kdo","language": "Czech languageCzech","description": "Biographies on notable [[CzechsCzech and [[Slovak people", "availability": true },{"name": "Österreichisches Biographisches Lexikon 1815–1950","language": "German languageGerman","description": "Biographies on notable Austrians", "availability": true },{"name": "Oxford Dictionary of National Biography","language": "English","description": "Comprehensive 66-volume reference work on notable figures from British history", "availability": true},{"name": "Dictionary of New Zealand Biography","language": "English, [[Māori languageMāori","description": "Entries on notable New Zealanders who have died", "availability": true },{"name": "Dizionario Biografico degli Italiani","language": "Italian","description": "Biographical Dictionary of Italian People, published by the [[Istituto dell'Enciclopedia ItalianaIstituto dell'Enciclopedia Italiana Treccani", "availability": true }], function(err, results) {
+    // dbo.collection('biography').insertMany([{"name": "Australian Dictionary of Biography","language": "English","description": "Entries on notable Australians who have died", "availability": true, "studentId": "" },{"name": "Croatian Biographical Lexicon","language": "Croatian languageCroatian","description": "Multi-volume biographical reference work on notable figures from Croatian history", "availability": true, "studentId": "" },{"name": "Deutsche Biographie","language": "German languageGerman","description": "Biographies on notable German speaking people", "availability": true, "studentId": "" },{"name": "Dictionary of Canadian Biography","language": "English, French","description": "Entries on notable Canadians", "availability": true, "studentId": "" },{"name": "Dictionary of Irish Architects","language": "English","description": "Biographical information on Irish architects from 1720 to 1940","availability": true, "studentId": "" },{"name": "Kdo byl kdo","language": "Czech languageCzech","description": "Biographies on notable [[CzechsCzech and [[Slovak people", "availability": true, "studentId": "" },{"name": "Österreichisches Biographisches Lexikon 1815–1950","language": "German languageGerman","description": "Biographies on notable Austrians", "availability": true, "studentId": "" },{"name": "Oxford Dictionary of National Biography","language": "English","description": "Comprehensive 66-volume reference work on notable figures from British history", "availability": true, "studentId": ""},{"name": "Dictionary of New Zealand Biography","language": "English, [[Māori languageMāori","description": "Entries on notable New Zealanders who have died", "availability": true, "studentId": "" },{"name": "Dizionario Biografico degli Italiani","language": "Italian","description": "Biographical Dictionary of Italian People, published by the [[Istituto dell'Enciclopedia ItalianaIstituto dell'Enciclopedia Italiana Treccani", "availability": true, "studentId": "" }], function(err, results) {
+    //     if (err) throw err;
     //     console.log(results.insertedCount + " documents inserted successfully");
-    //     database.close();
     // });
 });
