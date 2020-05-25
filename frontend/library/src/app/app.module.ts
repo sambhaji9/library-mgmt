c59add3dabe7df7cbb48003dfc00c6c026cbdac8
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-05-25 20:28:10 +0200

    Added a new form for new subject area, modified the route

--- frontend/library/src/app/app.module.ts
+++ frontend/library/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+
 import { BooksModule } from './books/books.module';
 import { StudentModule } from './student/student.module';
 
--- frontend/library/src/app/books/books.module.ts
+++ frontend/library/src/app/books/books.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksListComponent } from './books-list/books-list.component';
 import { SubjectAreasComponent } from './subject-areas/subject-areas.component';
-import { FormsModule } from '@angular/forms';
 import { UtilityPipesModule } from '../utility-pipes/utility-pipes.module';
 import { BooksFormComponent } from './books-form/books-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
 	declarations: [BooksListComponent, SubjectAreasComponent, BooksFormComponent],
 	imports: [
-		CommonModule, FormsModule, UtilityPipesModule
+		CommonModule, FormsModule, UtilityPipesModule, ReactiveFormsModule, RouterModule
 	],
-	exports: [BooksListComponent, SubjectAreasComponent]
+	exports: [BooksListComponent, SubjectAreasComponent, BooksFormComponent]
 })
 export class BooksModule { }
